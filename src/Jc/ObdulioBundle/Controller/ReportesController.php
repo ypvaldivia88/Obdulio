c8a5019628000e63de86f2811eb38698a5c274d7
ferro1989 <danielferrom@gmail.com>
2019-11-18 17:49:50 -0430

    actualizando proyecto

--- src/Jc/ObdulioBundle/Controller/ReportesController.php
+++ src/Jc/ObdulioBundle/Controller/ReportesController.php
@@ -3,14 +3,27 @@
 namespace Jc\ObdulioBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Doctrine\ORM\Query\ResultSetMapping;
 
 class ReportesController extends Controller
 {
     public function operativoAction(){
         if($this->getUser()==NULL){return $this->redirectToRoute('rh_usuarios_login');}
-        $em = $this->getDoctrine()->getManager();          
+        $em = $this->getDoctrine()->getManager();   
         
-        return $this->render('JcObdulioBundle:Reportes:operativo.html.twig');
+        $rsm = new ResultSetMapping();
+
+        $query = $em->createNativeQuery('SELECT username FROM usuarios', $rsm);
+        $users = $query->getResult();
+        
+        //listado de unidades
+        $articulos = $em->getRepository('JcObdulioBundle:Unidad')->findAll();
+        //listado de tipos de productos
+        $tipos = $em->getRepository('JcObdulioBundle:Tipoproducto')->findAll();
+        //listado de productos
+        $productos = $em->getRepository('JcObdulioBundle:Producto')->findAll();
+        //plan mensual de cada unidad para cada producto
+        
+        return $this->render('JcObdulioBundle:Reportes:operativo.html.twig', array('usuarios' => $users));
     }
 }
