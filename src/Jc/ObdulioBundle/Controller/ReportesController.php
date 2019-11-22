616da0865a53f86d2de0caa32515f77f3c25f504
Yasmani Palmero <yasmany.07@gmail.com>
2019-11-21 22:47:31 -0600

    Se completo el listado de reportes orperativos

--- src/Jc/ObdulioBundle/Controller/ReportesController.php
+++ src/Jc/ObdulioBundle/Controller/ReportesController.php
@@ -9,45 +9,80 @@ class ReportesController extends Controller
 {
     public function operativoAction(){
         if($this->getUser()==NULL){return $this->redirectToRoute('rh_usuarios_login');}
-        
+
         $em = $this->getDoctrine()->getManager();
+
         $query = $em->createQuery(
             "SELECT
-                pr.valor,	
-                CASE MONTH(CURRENT_DATE())
-                    WHEN 1 THEN pl.enero 
-                    WHEN 2 THEN pl.febrero 
-                    WHEN 3 THEN pl.marzo 
-                    WHEN 4 THEN pl.abril 
-                    WHEN 5 THEN pl.mayo 
-                    WHEN 6 THEN pl.junio 
-                    WHEN 7 THEN pl.julio 
-                    WHEN 8 THEN pl.agosto 
-                    WHEN 9 THEN pl.septiembre 
-                    WHEN 10 THEN pl.octubre 
-                    WHEN 11 THEN pl.noviembre 
-                    ELSE pl.diciembre 
-                END,
-                p.nombre,
-                tp.nombre,
-                u.nombre
+                pr.valor AS real,	
+                pl.enero,
+                pl.febrero,
+                pl.marzo,
+                pl.abril,
+                pl.mayo,
+                pl.junio,
+                pl.julio,
+                pl.agosto,
+                pl.septiembre,
+                pl.octubre,
+                pl.noviembre,
+                pl.diciembre,
+                p.nombre AS producto,
+                tp.nombre AS tipo,
+                u.nombre AS unidad
             FROM JcObdulioBundle:Produccion pr
-            LEFT JOIN JcObdulioBundle:Producto p ON pr.fk_producto = p.id
-            LEFT JOIN JcObdulioBundle:Planificacionproduccion pl ON pl.fk_producto = p.id
-            LEFT JOIN JcObdulioBundle:Tipoproducto tp ON p.fk_tipoproducto = tp.id
-            LEFT JOIN JcObdulioBundle:Unidad u ON pr.fk_unidad = u.id AND pl.fk_unidad = u.id"   
-        );
+            LEFT JOIN JcObdulioBundle:Producto p WHERE pr.fkProducto = p.id
+            LEFT JOIN JcObdulioBundle:Planificacionproduccion pl WHERE pl.fkProducto = p.id
+            LEFT JOIN JcObdulioBundle:Tipoproducto tp WHERE p.fkTipoproducto = tp.id
+            LEFT JOIN JcObdulioBundle:Unidad u WHERE pr.fkUnidad = u.id AND pl.fkUnidad = u.id"   
+        );        
+
+        $operativo = $query->getResult();      
+        $tiposProducto = $em->getRepository('JcObdulioBundle:Tipoproducto')->findAll();
+     
         
-        $reporte = $query->getResult();
+        return $this->render('JcObdulioBundle:Reportes:operativo.html.twig', array('operativo' => $operativo, 'tiposProducto' => $tiposProducto));
+    }
+
+    public function totalesAction($tipoProducto){
+        if($this->getUser()==NULL){return $this->redirectToRoute('rh_usuarios_login');}
+
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery(
+            "SELECT
+                Sum(produccion.valor) AS `real`,
+                tipoproducto.nombre AS tipo,
+                unidad.nombre AS unidad,
+                Sum(planificacionproduccion.enero) AS ene,
+                Sum(planificacionproduccion.febrero) AS feb,
+                Sum(planificacionproduccion.marzo) AS mar,
+                Sum(planificacionproduccion.abril) AS abr,
+                Sum(planificacionproduccion.mayo) AS may,
+                Sum(planificacionproduccion.junio) AS jun,
+                Sum(planificacionproduccion.julio) AS jul,
+                Sum(planificacionproduccion.agosto) AS ago,
+                Sum(planificacionproduccion.septiembre) AS sep,
+                Sum(planificacionproduccion.octubre) AS oct,
+                Sum(planificacionproduccion.noviembre) AS nov,
+                Sum(planificacionproduccion.diciembre) AS dic,
+                planificacionproduccion.`año`
+                FROM
+                produccion
+                INNER JOIN producto ON produccion.fk_producto = producto.id
+                INNER JOIN tipoproducto ON producto.fk_tipoproducto = tipoproducto.id
+                INNER JOIN unidad ON produccion.fk_unidad = unidad.id
+                INNER JOIN planificacionproduccion ON planificacionproduccion.fk_producto = producto.id AND planificacionproduccion.fk_unidad = unidad.id
+                WHERE
+                tipoproducto.id = 2
+                GROUP BY
+                tipoproducto.nombre,
+                unidad.nombre,
+                planificacionproduccion.`año`"   
+        );        
         
-        /* //listado de unidades
-        $articulos = $em->getRepository('JcObdulioBundle:Unidad')->findAll();
-        //listado de tipos de productos
-        $tipos = $em->getRepository('JcObdulioBundle:Tipoproducto')->findAll();
-        //listado de productos
-        $productos = $em->getRepository('JcObdulioBundle:Producto')->findAll();
-        //plan mensual de cada unidad para cada producto */
+        $listado = $query->getResult();           
         
-        return $this->render('JcObdulioBundle:Reportes:operativo.html.twig', array('reporte' => $reporte));
+        return $this->render('JcObdulioBundle:Reportes:totales.html.twig', array('listado' => $listado));
     }
 }
